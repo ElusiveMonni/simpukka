07c35ca73efbe0653885fc5282a92d9026111dea
iraaz4321 <69959972+iraaz4321@users.noreply.github.com>
2023-10-28 21:47:21 +0300

    Added support for non local ray clusters

--- src/simpukka/initialise.py
+++ src/simpukka/initialise.py
@@ -3,9 +3,12 @@ import pathlib
 from simpukka import template
 
 
-def init_simpukka():
-    ray.init(runtime_env={"working_dir": pathlib.Path(__file__).parent,
+def init_simpukka(remote_cluster=None):
+    if remote_cluster is None:
+        ray.init(runtime_env={"working_dir": pathlib.Path(__file__).parent,
                           "eager_install": True})
-
+    else:
+        ray.init(remote_cluster)
     # Work around for issue with first task taking long
     template.Template("Init task").start()
+
