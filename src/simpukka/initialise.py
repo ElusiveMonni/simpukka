7047b517d36696542fa41644d56a905898e72ba2
iraaz4321 <69959972+iraaz4321@users.noreply.github.com>
2023-11-08 08:53:22 +0200

    added way of connecting to a remote cluster

--- src/simpukka/initialise.py
+++ src/simpukka/initialise.py
@@ -1,14 +1,37 @@
 import ray
 import pathlib
+import os
 from simpukka import template
+from sshtunnel import SSHTunnelForwarder
+
+def setup_tunnel():
+
+    remote_user = os.getenv("ray_server_user")
+    remote_password = os.getenv("ray_server_password")
+    remote_host = os.getenv("ray_server_host")
+    remote_port = 22
+    local_host = '127.0.0.1'
+
+    server = SSHTunnelForwarder(
+        (remote_host, remote_port),
+        ssh_username=remote_user,
+        ssh_password=remote_password,
+        remote_bind_address=("0.0.0.0", 10001),
+        local_bind_address=(local_host, 10001),
+    )
+
+    server.start()
 
 
-def init_simpukka(remote_cluster=None):
-    if remote_cluster is None:
+def init_simpukka(remote_cluster=None, tunnel_required=True):
+    if tunnel_required:
+            setup_tunnel()
+            ray.init("ray://127.0.0.1:10001")
+    elif remote_cluster is None:
         ray.init(runtime_env={"working_dir": pathlib.Path(__file__).parent,
                           "eager_install": True})
     else:
         ray.init(remote_cluster)
+
     # Work around for issue with first task taking long
     template.Template("Init task").start()
-
